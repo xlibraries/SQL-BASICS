50debaa019450f4c80ea583e65f51e7738d673b0
Xlib <64978693+xlibraries@users.noreply.github.com>
2022-03-22 13:15:51 +0530

    Added Constrains section with erros😅

--- SQL-BASICS.sql
+++ SQL-BASICS.sql
@@ -34,16 +34,9 @@ ALTER TABLE STUDENT ALTER COLUMN SID int NOT NULL;
 ALTER TABLE STUDENT ADD PRIMARY KEY(SID);
 
 
---Adding Primary key in Student table
-ALTER TABLE STUDENT ALTER COLUMN SID int NOT NULL;
-ALTER TABLE STUDENT ADD PRIMARY KEY(SID);
-
-
-SELECT * FROM STUDENT FOR XML PATH('STUDENT'), ROOT('STUDENT') 
-SELECT * FROM STUDENT FOR XML RAW 
-SELECT * FROM STUDENT FOR XML AUTO
-SELECT * FROM STUDENT FOR XML EXPLICIT
-
+select * from STUDENT FOR XML RAW
+select * from STUDENT FOR XML PATH('STUDENT'),ROOT('STUDENT')
+select * from STUDENT FOR XML AUTO
 
 DECLARE @XMLCONTENT AS XML
 SET @XMLCONTENT = (
@@ -168,3 +161,138 @@ FROM @xlmstudent.nodes('STUDENT/STUDENTS') STUDENT(C)
 SELECT T.C.query('.') AS result
 FROM @xlmstudent.nodes('STUDENT/STUDENTS') T(C)
 ORDER BY T.C.value('NAME[1]','VARCHAR(25)')
+
+--XML DATA TYPE IS DIRECTLY PASSED INTO QUERY
+USE SQLBASICS
+CREATE TABLE EMPLOYEEXML(EMPID INT PRIMARY KEY, EMP_DATA XML NOT NULL)
+SELECT * FROM EMPLOYEEXML
+INSERT INTO EMPLOYEEXML VALUES(1,'<EMPLOYEE EMPID = "101"><EMP_NAME>Xlib</EMP_NAME><LOCATION>"HAVANA"</LOCATION></EMPLOYEE>');
+INSERT INTO EMPLOYEEXML VALUES(2,'<EMPLOYEE EMPID = "102"><EMP_NAME>Ztip</EMP_NAME><LOCATION>"Merar"</LOCATION></EMPLOYEE>');
+INSERT INTO EMPLOYEEXML VALUES(3,'<EMPLOYEE EMPID = "103"><EMP_NAME>Hola</EMP_NAME><LOCATION>"Guvava"</LOCATION></EMPLOYEE>');
+INSERT INTO EMPLOYEEXML VALUES(4,'<EMPLOYEE EMPID = "104"><EMP_NAME>Bhola</EMP_NAME><LOCATION>"Sholapur"</LOCATION></EMPLOYEE>');
+
+----for query
+	--select ed.EMP_DATA.query('(/EMPLOYEE/EMP_NAME)[1]') as EMPLOYEE_NAME
+	--FROM EmployeeXML ed;
+----for value
+	--select ed.EMP_DATA.value('(/EMPLOYEE/EMP_NAME)[1]','varchar(25)') as EMPLOYEE_NAME
+	--FROM EmployeeXML ed;
+----for exist
+	--select ed.EMP_DATA.exist('(/EMPLOYEE/EMP_NAME)[1]') as EMPLOYEE_NAME
+	--FROM EmployeeXML ed
+	--where ed.EMPID=102;
+----for update
+
+----for 
+
+
+--Nodes
+select ed.EMP_DATA.query('(/EMPLOYEE/EMP_NAME)[1]') as EMPLOYEE_NAME
+FROM EmployeeXML ed
+CROSS APPLY EMP_DATA.nodes('(EMPLOYEE)') AS MYNODES(EMP_DATA)
+
+--UPDATE delete
+UPDATE EMPLOYEEXML
+SET EMP_DATA.modify('delete(/EMPLOYEE)[1]');
+--UPDATE INSERT
+UPDATE EMPLOYEEXML
+SET EMP_DATA.modify('insert<EMPLOYEE EMPID = "101"><EMP_NAME>Xlib</EMP_NAME><LOCATION>"HAVANA"</LOCATION></EMPLOYEE> after (/EMPLOYEE[1])')
+WHERE EMPLOYEEXML.EMPID = 1;
+
+--Module 4 Indexes(process to order unstructured data)
+/* Index Two types clustured and non-clustured
+example:- Clustered-> page numbe; Non Clustured-> Groups/ References*/
+
+--Clustured
+CREATE TABLE EmployeeDetails
+(
+	EmpId int NOT NULL,
+	PassportNo VarChar(50) Null,
+	ExpiryDate VarChar(25) Null
+)
+INSERT INTO EmployeeDetails VALUES(1,'AASASAA',Null)
+INSERT INTO EmployeeDetails VALUES(2,'AASGRAA',Null)
+INSERT INTO EmployeeDetails VALUES(7,'AGRGSAA',Null)
+INSERT INTO EmployeeDetails VALUES(8,'AHRRSAA',Null)
+INSERT INTO EmployeeDetails VALUES(5,'AHTRSAA',Null)
+
+SELECT * FROM EmployeeDetails
+CREATE CLUSTERED INDEX CIX_EmpDetails
+ON EmployeeDetails(EmpId)
+
+--Non-Clustures
+CREATE TABLE EmployeeDetailsNC
+(
+	EmpId int NOT NULL,
+	EmpName VarChar(50) Null,
+	EmailId VarChar(25) Null
+)
+INSERT INTO EmployeeDetailsNC VALUES(1,'Xlib','Xlib@gmail.com')
+INSERT INTO EmployeeDetailsNC VALUES(2,'Maya','Maya@gmail.com')
+INSERT INTO EmployeeDetailsNC VALUES(7,'Gaya','Gaya@gmail.com')
+INSERT INTO EmployeeDetailsNC VALUES(9,'B0la','Bola@gmail.com')
+INSERT INTO EmployeeDetailsNC VALUES(5,'Bhola','Bhola@gmail.com')
+
+SELECT * FROM EmployeeDetailsNC
+
+CREATE NONCLUSTERED INDEX NCI_EmpDetails
+ON EmployeeDetailsNC(EmpId)
+--Deleting index
+DROP INDEX NCI_EmpDetails
+ON EmployeeDetailsNC
+--Alter
+ALTER INDEX NCI_EmpDetails
+ON EmployeeDetailsNC
+SET(IGNORE_DUP_KEY = OFF)
+
+
+
+-- Isko waapas karna hai
+
+
+--Constraints
+/*
+Primary key:- 
+	uniquely identify each row in a table
+	not null
+	no null value
+*/
+--Set Primary Key
+CREATE TABLE EmployeeTable
+(
+	EmpId int NOT NULL,
+	EmpName VarChar(50) Null,
+	EmailId VarChar(25) Null,
+	Salary int Null,
+	Department VarChar(25) Null
+);
+SELECT * FROM EmployeeTable
+INSERT INTO EmployeeDetails VALUES(1,'Xlib','Xlib@gmail.com',70000,'.NET')
+INSERT INTO EmployeeDetails VALUES(2,'Maya','Maya@gmail.com',1000,'Java')
+INSERT INTO EmployeeDetails VALUES(3,'Gaya','Gaya@gmail.com',10100,'SF')
+INSERT INTO EmployeeDetails VALUES(4,'B0la','Bola@gmail.com',10400,'HR')
+INSERT INTO EmployeeDetails VALUES(4,'Bhola','Bhola@gmail.com',40000,'CPO')
+
+--Add Constrains
+ALTER TABLE EmployeeTable
+ADD CONSTRAINT PK_EmpId PRIMARY KEY(EmpId) 
+
+--DropPrimary Key
+ALTER TABLE EmployeeTable
+DROP CONSTRAINT EmpId;
+
+--Another Example  DEPARTMENT(DEPTID PK,DEPTNAME)
+CREATE TABLE DepartmentTable
+(
+	DepId int Not Null,	
+	DepName VarChar(25),
+
+	CONSTRAINT PK_DepId PRIMARY KEY(DepId)
+)
+
+--Set foreign key
+ALTER TABLE EmployeeTable
+ADD CONSTRAINT FK_DID FOREIGN KEY(DepID)
+REFERENCES Department(DepId)
+
+--Drop foreign  key
